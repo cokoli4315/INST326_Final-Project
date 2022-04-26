01853b505d31291f9f722843ef26267c89805ae8
mshay1 <94201988+mshay1@users.noreply.github.com>
2022-04-26 14:35:55 -0400

    Update inst326_final_project.py
    Updated with comments on who is doing what work in program.

--- inst326_final_project.py
+++ inst326_final_project.py
@@ -5,10 +5,13 @@ Assignment: INST326 Final Project
 Date: 4_14_22
 Challenges Encountered: 
 """
+# by Friday, 4/29: have Actor class mostly done, find_actor_name/page mostly done, & publish_comment mostly done
 from imdb.Person import Person
 from imdb import Cinemagoer
 import praw
+import re
 
+# McKenna & Declan
 class Actor:
     """Captures and holds information about an actor using an IMDB page.
     
@@ -55,7 +58,7 @@ class Actor:
         """
         pass
     
-def get_post(reddit, subreddit):
+def get_post_title(reddit, subreddit):
     """Accesses each post in r/movies.
     
     Args:
@@ -69,10 +72,7 @@ def get_post(reddit, subreddit):
     movies_sub = reddit.subreddit("movies")
     #goes through 10 most recent posts on r/movies and prints info about them, not in final just an example
     for submission in movies_sub.new(limit=10):
-        print(submission.title)
-        print(submission.score)
-        print(submission.id) # return this, fix for final
-        print(submission.url)
+        return submission.title, submission.id # return this, fix for final
 
 def get_title(post_id):
     """Accesses the title of each post. 
@@ -84,7 +84,8 @@ def get_title(post_id):
         post_title (str): the title of the post
     """
     return post_id.title
-        
+
+# Chikezie        
 def find_actor_name(post_title):
     """Looks for an actor's name in the title of a post using regex.
     
@@ -94,8 +95,11 @@ def find_actor_name(post_title):
     Returns:
         actor_name (str): the name of the actor if found
     """
+    # sep title w/ spaces, loop thru title, once a regex match is found call find_actor_page(), 
+    # if f_a_p() does not find a match continue looking thru, title for a name until you reach the end
     pass
 
+# Chikezie
 def find_actor_page(actor_name):
     """Looks through IMDB for the actor's page.
     
@@ -107,6 +111,7 @@ def find_actor_page(actor_name):
     """
     pass
 
+# Surafel
 def create_comment(actor):
     """Creates a string comment with an Actor object's attributes.
     
@@ -116,8 +121,10 @@ def create_comment(actor):
     Returns:
         comment (str): hold all the info about the actor parameter
     """
+    # ex: f"Actor's Name: {actor.name}\n Actor's Age: {} \n"
     pass
 
+# Surafel
 def publish_comment(post_id, comment):
     """Comments a string to a Reddit post.
     
@@ -125,8 +132,10 @@ def publish_comment(post_id, comment):
         post_id (int): the ID of the post that has the actor's name in it
         comment (str): the comment that will be published to the post
     """
+    # take comment & publish it to reddit post_id as a comment (look at Reddit API on how to do that)
     pass
 
+# McKenna & Declan
 def main():
     """Runs the entire program. Calls get_posts(), calls get_title() for each post, calls find_actor_name() for title, 
     if an actor's name is found calls find_actor_page(), if a page for the actor is found creates an Actor instance, 
@@ -139,6 +148,11 @@ def main():
         username="my username",
         password="my password",
     )
+    
+    movies_sub = reddit.subreddit("movies")
+    for submission in movies_sub.new(limit=10):
+        call get_post_title()
+        
 
 if __name__ == "__main__":
     # calls main to run program
